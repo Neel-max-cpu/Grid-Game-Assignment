55f30c0ee2da272dc736620acde2b1a74d43390b
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-05-05 09:57:43 +0530

    Refactor next.config.ts to simplify configuration
    Removed eslint configuration from nextConfig.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-  reactCompiler: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+const nextConfig: NextConfig = {
+   reactCompiler: true  
 };
 
 export default nextConfig;
