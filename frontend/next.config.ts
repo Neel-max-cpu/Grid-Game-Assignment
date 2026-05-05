16e76f874ecaa6ae420c2f798a6b58662bb4db64
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-05-05 08:51:46 +0530

    es lint :memo:

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
   reactCompiler: true,
-  ignoreDuringBuilds: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
