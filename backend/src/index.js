c3d5add84a7b15cc030f14216889f5e2d13dafe1
Neel Bhattacharya <neelbhatta22@gmail.com>
2026-05-05 08:44:15 +0530

    update :memo:

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
     res.send("Health check - Server is running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
